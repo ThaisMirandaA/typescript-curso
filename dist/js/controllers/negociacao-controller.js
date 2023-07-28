5972786dabbbf5a0cf9654bd4d9a4bfd5839b84f
Thais <thaismirandadealmeida@gmail.com>
2023-07-28 18:39:27 -0300

    primero commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,29 @@
+import { Negociacoes } from "../models/listaNegociacoes.js";
+import { Negociacao } from "../models/negociacao.js";
+export class Negociacaocontroller {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
+        this.limpaFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(data, quantidade, valor);
+    }
+    limpaFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
